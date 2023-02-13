32e68cdc2cc3c3cb9479ee729f77795c6e8be6ce
elisjudge <101382345+elisjudge@users.noreply.github.com>
2023-02-13 14:05:14 +0900

    Added row functionality

--- src/database.py
+++ src/database.py
@@ -1,30 +1,62 @@
 import sqlite3
 import os.path
-import sys
+import datetime
 
 class Database:
     
-    def __init__(self) -> None:
-        self.dbname = 'db.db'
-        if not os.path.exists(f'./data/{self.dbname}'):
-            self.create_database()
+    def __init__(self, dbname = None) -> None:
+        self.dbname = 'db.db' if not dbname else dbname
+                
+        if not os.path.exists('./data'):
+            os.makedirs('./data') 
+        self.create_connection()
+
+    def create_connection(self) -> None:
+        try:
+            self.connection = sqlite3.connect(f'./data/{self.dbname}')
+            self.cursor = self.connection.cursor()
+            self.cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS TICKER (
+                    date DATETIME NOT NULL PRIMARY KEY,
+                    open REAL NOT NULL,
+                    high REAL NOT NULL,
+                    low REAL NOT NULL,
+                    close REAL NOT NULL)  
+                    """)
+            self.connection.commit()
+        except Exception as e:
+            print(f"Error creating connection to database: {e}")
+    
+    def close_connection(self) -> None:
+        self.cursor.close()
+    
+    def add_row(self, date: datetime.date, open: float, high: float, low: float, close: float) -> None:
+        try:
+            self.cursor.execute("""
+                INSERT INTO TICKER (date, open, high, low, close)
+                VALUES (?, ?, ?, ?, ?)
+                """, (date, open, high, low, close))
+        except Exception as e:
+            print(f"Error adding row to table: {e}")
+        self.connection.commit()
+
+    def delete_row(self, date: datetime.date):
+        try:
+            date_str = date.strftime('%Y-%m-%d')
+            self.cursor.execute("""
+            DELETE FROM TICKER WHERE date = ?          
+            """, (date_str,))
+        except Exception as e:
+            print(f"Error deleting row from table: {e} ")
+        self.connection.commit()
+
+    def update_row(self, date: datetime, open: float, high: float, low: float, close: float) -> None:
+        pass
+
+    def __del__(self):
+        self.close_connection()
 
 
-    def create_database(self):
-        connection = sqlite3.connect(f'./data/{self.dbname}')
-        cursor = connection.cursor()
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS AUDJPY (
-        date NOT NULL PRIMARY KEY,
-        open NOT NULL,
-        high NOT NULL,
-        low NOT NULL,
-        close NOT NULL)  
-        """)
-
-        connection.commit()
-        cursor.close()
 
 
-print(sys.path)
+
